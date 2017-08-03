21d94d2491e633edaa2c20752990fec78fc91777
yzijun <yzijun@126.com>
2017-08-03 16:38:35 +0800

    修改user表定义

--- src/lib/sequelize.js
+++ src/lib/sequelize.js
@@ -4,6 +4,10 @@ import { DB as DBConfig, System as SystemConfig } from '../config'
 const db = new Sequelize(DBConfig.database, DBConfig.username, DBConfig.password, {
   host: DBConfig.host,
   dialect: SystemConfig.db_type,
+  define: {
+    // 字段以下划线（_）来分割（默认是驼峰命名风格）
+    underscored: true
+  },
   dialectOptions: { // MySQL > 5.5，其它数据库删除此项
     charset: 'utf8mb4',
     collate: 'utf8mb4_unicode_520_ci',
@@ -12,7 +16,7 @@ const db = new Sequelize(DBConfig.database, DBConfig.username, DBConfig.password
   },
   pool: {
     // max: 50, // 默认设置
-    max: 1, // 测试用1就可以
+    max: 1, // 测试用1
     min: 0,
     idle: 10000
   }
--- src/models/user.js
+++ src/models/user.js
@@ -1,19 +1,49 @@
 import {Sequelize, db} from '../lib/sequelize'
+import moment from 'moment'
 
 export const User = db.define('api_user', {
-  firstName: {
+  username: {
+    type: Sequelize.STRING(50),
+    unique: true
+  },
+  password: {
     type: Sequelize.STRING
   },
-  lastName: {
-    type: Sequelize.STRING
+  email: {
+    type: Sequelize.STRING(50)
   },
-  createdAt: {
-    type: Sequelize.BIGINT
+  created_at: {
+    type: Sequelize.STRING(30)
   },
-  updatedAt: {
+  updated_at: {
+    type: Sequelize.STRING(30)
+  },
+  is_delete: {
+    type: Sequelize.BOOLEAN,
+    defaultValue: false
+  },
+  timestamp_at: {
     type: Sequelize.BIGINT
   }
 },
 {
-  timestamps: false
+  timestamps: false,
+  comment: '用户信息表'
+})
+// 创建表结构
+// User.sync({force: true})
+
+// force: true will drop the table if it already exists
+// 创建表结构并且增加一条数据
+User.sync({force: true}).then(() => {
+  // Table created
+  return User.create({
+    username: '小明',
+    password: 'Hancock',
+    email: 'xiao@163.com',
+    created_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+    updated_at: moment().format('YYYY-MM-DD HH:mm:ss'),
+    is_delete: false,
+    timestamp_at: moment().format('X')
+  })
 })
