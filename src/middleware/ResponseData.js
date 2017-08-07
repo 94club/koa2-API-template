e2a0f4332f019b709c29d1dbc613ba5e6493eab7
yzijun <yzijun@126.com>
2017-08-07 17:30:47 +0800

    修改返回json数据的名称

--- src/middleware/ResponseData.js
+++ src/middleware/ResponseData.js
@@ -2,26 +2,26 @@
  * 返回给客户端的状态、消息和json数据
  */
 export default async (ctx, next) => {
-  ctx.error = (message = '', errMsg = '', data = {}, code = 500) => {
+  ctx.error = (message = '', errMsg = '', result = {}, code = 500) => {
     ctx.body = {
       code,
       message,
       errMsg: '用于开发人员方便错误调试。服务器出错,错误信息：' + errMsg,
-      data
+      result
     }
   }
-  ctx.notFound = (message, data = {}) => {
+  ctx.notFound = (message, result = {}) => {
     ctx.body = {
       code: 404,
       message,
-      data
+      result
     }
   }
-  ctx.success = (data, message = 'ok') => {
+  ctx.success = (result, message = 'ok') => {
     ctx.body = {
       code: 200,
       message,
-      data
+      result
     }
   }
   await next()
