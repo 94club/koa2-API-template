96f0c81c14a42129ae994d13ff6ae3d6403f0a2c
yzijun <yzijun@126.com>
2017-08-18 10:37:08 +0800

    增加关联关系

--- src/models/index.js
+++ src/models/index.js
@@ -2,13 +2,16 @@
 let requireDirectory = require('require-directory')
 module.exports = requireDirectory(module)
 */
+
 import { db } from '../lib/sequelize'
 import User from './user.model'
 import FileUpload from './fileUpload.model'
 import TodoList from './todoList.model'
 
+TodoList.belongsTo(User)
 User.hasMany(TodoList, {
-//   foreignKey: 'user_id', // 可以自己指定外键，不设置时系统默认
+  // as: 'User', // 使用as选项为关系数据指定别名
+  // foreignKey: 'user_id', // TodoList表中增加user_id外键。可以自己指定外键名字，不设置时系统默认
   constraints: false // 不设置外键约束
 })
 // 重新创建所有的表结构
